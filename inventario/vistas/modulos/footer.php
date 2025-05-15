f254603d5555d406e5b45af8d8d24c4144c58a5f
Ismael <46927613+Abrio93@users.noreply.github.com>
2025-05-15 11:27:02 +0200

    Delete inventario directory

--- inventario/vistas/modulos/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="main-footer">
-    <strong>
-        <a href="http://plataforma.siges21.com">
-            <?php
-                echo "2º DAW - DEW - IES La Arboleda - Curso ".date("Y")."/".(date("Y")+1);
-            ?>
-        </a>
-    </strong>
-</footer>
