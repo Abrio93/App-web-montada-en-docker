609601fee8a479b5a7c89588b928e3b74e27432e
Ismael <ismaelabrio@gmail.com>
2025-09-23 14:19:06 +0200

    creando estructura para la parte pública

---
+++ html/vistas/modulos/inicio.php
@@ -0,0 +1,26 @@
+<div class="row">
+  <div class="col-md-1 text-center">
+    <h1>Inicio</h1>
+  </div>
+  <div class="col-md-10 text-center">
+    <div class="row mt-5">
+      <div class="col-md-4 text-center">
+        <div class="card img-borde-celeste rounded-3" style="width: 18rem;">
+          <div class="d-flex justify-content-center pt-3">
+            <img src="https://webevolmind.b-cdn.net/wp-content/uploads/2019/07/%C2%BFPor-que-es-util-la-imagen-como-recurso-educativo.webp" class="card-img-top img-redonda img-borde-celeste"alt="...">
+          </div>
+          <div class="card-body text-center">
+            <h5 class="card-title">Perfil</h5>
+            <p class="card-text">Aquí un poco de info tuya</p>
+            <a href="#" class="btn btn-primary">Ver perfil</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 text-center">hola2</div>
+      <div class="col-md-4 text-center">hola3</div>
+    </div>
+  </div>
+  <div class="col-md-1 text-center">
+    <h1>Fin</h1>
+  </div>
+</div>
