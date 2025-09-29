c46c70de07e504bc9bd65c2a6be8e8cc26e74479
Ismael <ismaelabrio@gmail.com>
2025-09-29 13:56:42 +0200

    creando página inicio

--- html/vistas/modulos/cabecera.php
+++ html/vistas/modulos/cabecera.php
@@ -1,28 +1,28 @@
 <nav class="navbar navbar-light navbar-expand-lg border border-dark border-1" style="background-color: #e3f2fd;">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">FriendBook</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Perfil</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="#">Explora</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
+            Ajustes
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Perfil</a></li>
+            <li><a class="dropdown-item" href="#">Notificaciones</a></li>
+            <li><a class="dropdown-item" href="#">General</a></li>
           </ul>
         </li>
       </ul>
--- html/vistas/modulos/inicio.php
+++ html/vistas/modulos/inicio.php
@@ -1,46 +1,65 @@
-<div class="row">
-  <div class="col-12 col-md-1 text-center">
-    <h1>Inicio</h1>
-  </div>
-  <div class="col-md-10 text-center">
-    <div class="row mt-5">
-      <div class="col-md-3 text-center">
-        <div class="card img-borde-celeste rounded-3" style="width: 22rem;">
-          <div class="d-flex justify-content-center pt-3">
-            <img src="https://webevolmind.b-cdn.net/wp-content/uploads/2019/07/%C2%BFPor-que-es-util-la-imagen-como-recurso-educativo.webp" class="card-img-top img-redonda img-borde-celeste" alt="...">
-          </div>
-          <div class="card-body text-center">
-            <h5 class="card-title">Perfil</h5>
-            <p class="card-text">Aquí un poco de info tuya</p>
-            <a href="#" class="btn btn-primary">Ver perfil</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-8 text-center">
-        <div class="card fondo-feed">
-          <div class="d-flex justify-content-center pt-3">
-            <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2e/af/33/14/el-mejor-bar-playero.jpg?w=900&h=500&s=1" class="card-img-top img-feed img-fluid px-2" alt="...">
-          </div>
-          <div class="card-body text-start">
-            <div class="row mb-2">
-              <div class="col-12">
-                Esto es el texto de la imagen, puede ser una descripción o lo que sea.
-              </div>
+<div class="container-fluid">
+  <div class="row">
+
+  <!-- Parte izquierda -->
+    <div class="col-md-2 text-center d-flex justify-content-center">
+      <div class="row">
+        <div class="col-12 mt-5" style="margin-left: 15%;">
+          <div class="card fondo-feed" style="width: 20rem;">
+            <div class=" pt-3">
+              <img src="https://webevolmind.b-cdn.net/wp-content/uploads/2019/07/%C2%BFPor-que-es-util-la-imagen-como-recurso-educativo.webp" class="card-img-top img-redonda " alt="...">
             </div>
-            <div class="row">
-              <div class="col-12">
-                <img src="vistas/img/plantilla/chat-heart.svg" width="25px" class="me-3 cursor" title="Me gusta">
-                <img src="vistas/img/plantilla/chat.svg" width="25px" class="me-3 cursor" title="Comentar">
-                <img src="vistas/img/plantilla/send.svg" width="25px" class="cursor" title="Compartir">
-              </div>
+            <div class="card-body text-center">
+              <h5 class="card-title">Perfil</h5>
+              <p class="card-text">Aquí un poco de info tuya</p>
+              <a href="#" class="btn btn-primary">Ver perfil</a>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-md-1 text-center">hola3</div>
     </div>
-  </div>
-  <div class="col-md-1 text-center">
-    <h1>Fin</h1>
+
+    <!-- Parte central/derecha -->
+    <div class="col-md-10">
+      <?php
+      //? LLAMAMOS A LAS PUBLICACIONES
+      $publicaciones = new ControladorPublicaciones();
+      $publicaciones = $publicaciones->ctrMostrarPublicaciones();
+
+      foreach ($publicaciones as $clave => $valor) {
+        $fotos = new ControladorFotos();
+        //? PASAMOS EL ID DE LA PUBLICACION PARA QUE NOS TRAIGA LAS FOTOS DE ESA PUBLICACION
+        $fotos = $fotos->ctrMostrarFotos($valor["id"]);
+
+      ?>
+        <div class="row">
+          <div class="col-1"></div>
+          <div class="col-md-10 my-5 text-center">
+            <div class="card fondo-feed">
+              <div class="d-flex justify-content-center pt-3">
+                <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2e/af/33/14/el-mejor-bar-playero.jpg?w=900&h=500&s=1" class="card-img-top img-feed img-fluid px-2" alt="...">
+              </div>
+              <div class="card-body text-start">
+                <div class="row mb-2">
+                  <div class="col-12">
+                    <?php echo $valor["texto"]; ?>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-12">
+                    <img src="vistas/img/plantilla/heart.svg" width="25px" class="me-3 cursor" title="Me gusta">
+                    <img src="vistas/img/plantilla/chat.svg" width="25px" class="me-3 cursor" title="Comentar">
+                    <img src="vistas/img/plantilla/send.svg" width="25px" class="cursor" title="Compartir">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-1"></div>
+        </div>
+      <?php
+      }
+      ?>
+    </div>
   </div>
 </div>
