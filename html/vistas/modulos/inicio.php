014e654027f48f885a52cf96ec2aba8a527c768e
Ismael <ismaelabrio@gmail.com>
2025-09-29 08:39:45 +0200

    subida genérica

--- html/vistas/modulos/inicio.php
+++ html/vistas/modulos/inicio.php
@@ -1,13 +1,13 @@
 <div class="row">
-  <div class="col-md-1 text-center">
+  <div class="col-12 col-md-1 text-center">
     <h1>Inicio</h1>
   </div>
   <div class="col-md-10 text-center">
     <div class="row mt-5">
-      <div class="col-md-4 text-center">
-        <div class="card img-borde-celeste rounded-3" style="width: 18rem;">
+      <div class="col-md-3 text-center">
+        <div class="card img-borde-celeste rounded-3" style="width: 22rem;">
           <div class="d-flex justify-content-center pt-3">
-            <img src="https://webevolmind.b-cdn.net/wp-content/uploads/2019/07/%C2%BFPor-que-es-util-la-imagen-como-recurso-educativo.webp" class="card-img-top img-redonda img-borde-celeste"alt="...">
+            <img src="https://webevolmind.b-cdn.net/wp-content/uploads/2019/07/%C2%BFPor-que-es-util-la-imagen-como-recurso-educativo.webp" class="card-img-top img-redonda img-borde-celeste" alt="...">
           </div>
           <div class="card-body text-center">
             <h5 class="card-title">Perfil</h5>
@@ -16,8 +16,28 @@
           </div>
         </div>
       </div>
-      <div class="col-md-4 text-center">hola2</div>
-      <div class="col-md-4 text-center">hola3</div>
+      <div class="col-md-8 text-center">
+        <div class="card fondo-feed">
+          <div class="d-flex justify-content-center pt-3">
+            <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2e/af/33/14/el-mejor-bar-playero.jpg?w=900&h=500&s=1" class="card-img-top img-feed img-fluid px-2" alt="...">
+          </div>
+          <div class="card-body text-start">
+            <div class="row mb-2">
+              <div class="col-12">
+                Esto es el texto de la imagen, puede ser una descripción o lo que sea.
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <img src="vistas/img/plantilla/chat-heart.svg" width="25px" class="me-3 cursor" title="Me gusta">
+                <img src="vistas/img/plantilla/chat.svg" width="25px" class="me-3 cursor" title="Comentar">
+                <img src="vistas/img/plantilla/send.svg" width="25px" class="cursor" title="Compartir">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-1 text-center">hola3</div>
     </div>
   </div>
   <div class="col-md-1 text-center">
